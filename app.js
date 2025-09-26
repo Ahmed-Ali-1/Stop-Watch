262473ebab4640dd8debf9681c55adce077751ff
Ahmed-Ali-1 <ahmedali034012449@gmail.com>
2025-09-27 01:02:45 +0500

    Add Hour

--- app.js
+++ app.js
@@ -2,27 +2,39 @@ var getMinute = document.getElementById("minute")
 var getSecond = document.getElementById("second")
 var getMilisecond = document.getElementById("milisecond")
 
+var getHour = document.getElementById("hour")
+
 var startButton = document.getElementById("startBtn")
 
 var minute = 0
 var second = 0
 var milisecond = 0
+var hour = 0
 
 var interval;
 
 function start() {
     interval = setInterval(function () {
-        getMilisecond.innerText = milisecond
+
+        if (milisecond < 10) {
+            getMilisecond.innerText = "0" + milisecond
+        } else {
+            getMilisecond.innerText = milisecond
+
+        }
+
         if (getMilisecond.innerText == 100) {
+            second++
             if (second < 10) {
                 getSecond.innerText = "0" + second
             } else {
                 getSecond.innerText = second
             }
-            second++
             milisecond = 0
+
         }
-        if (second == 6) {
+
+        if (second == 61) {
             minute++
             if (minute < 10) {
                 getMinute.innerText = "0" + minute
@@ -30,19 +42,40 @@ function start() {
                 getMinute.innerText = minute
             }
             second = 0
+
         }
+
+        if (minute == 60) {
+            hour++
+            if (hour < 10) {
+                getHour.innerHTML = "0" + hour
+            } else {
+                getHour.innerText = hour
+            }
+            minute = 0
+
+        }
+
+        if (getHour.innerText == 24) {
+            hour = 0
+
+        }
+
         milisecond++
     }, 10);
-    startBtn.disabled = true
+
+    startButton.disabled = true
 }
 
 function stop() {
     clearInterval(interval)
-    startBtn.disabled = false
+    startButton.disabled = false
 }
+
 function reset() {
     clearInterval(interval)
     getMinute.innerText = "00"
     getSecond.innerText = "00"
     getMilisecond.innerText = "00"
+    getHour.innerText = "00"
 }
